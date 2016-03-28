aa7ab44727271635752ade7bd80c0dc89167a2dd
Kees Bakker <kees@sodaq.com>
2016-03-28 20:50:40 +0200

    In the test program do more of the short delay tests

--- examples/test_ublox_gps/test_ublox_gps.ino
+++ examples/test_ublox_gps/test_ublox_gps.ino
@@ -7,8 +7,19 @@
 // List of interval values to be used in loop()
 // to measure how long it takes to get a fix.
 uint32_t intervals[] = {
+
+        // Do a few tests with 1 minute delay
         1UL * 60 * 1000,
+        1UL * 60 * 1000,
+        1UL * 60 * 1000,
+
+        // Try a few longer delays
+        2UL * 60 * 1000,
+        2UL * 60 * 1000,
         5UL * 60 * 1000,
+        5UL * 60 * 1000,
+
+        // Slowly increase the delays
         15UL * 60 * 1000,
         30UL * 60 * 1000,
         1UL * 60 * 60 * 1000,
@@ -18,6 +29,7 @@ uint32_t intervals[] = {
 };
 size_t interval_ix = 0;
 
+void find_fix(uint32_t delay_until);
 void do_flash_led(int pin);
 
 void setup()
@@ -46,20 +58,37 @@ void setup()
 
     // This is for debugging to see more details, more messages
     // Use this in combination with setDiag()
-    // sodaq_gps.setMinNumOfLines(10);
+    //sodaq_gps.setMinNumOfLines(10);
 
     // Uncomment the next line if you want to see the incoming $GPxxx messages
-    // sodaq_gps.setDiag(MySerial);
+    //sodaq_gps.setDiag(MySerial);
+
+    // First time finding a fix
+    find_fix(0);
 }
 
 void loop()
 {
-    uint32_t start = millis();
+    uint32_t wait_ms = intervals[interval_ix];
+    if (++interval_ix > ARRAY_DIM(intervals)) {
+        interval_ix = 0;
+    }
+    find_fix(wait_ms);
+}
 
+/*!
+ * Find a GPS fix, but first wait a while
+ */
+void find_fix(uint32_t delay_until)
+{
+    MySerial.println(String("delay ... ") + delay_until + String("ms"));
+    delay(delay_until);
+
+    uint32_t start = millis();
     uint32_t timeout = 900L * 1000;
     MySerial.println(String("waiting for fix ..., timeout=") + timeout + String("ms"));
     if (sodaq_gps.scan(false, timeout)) {
-        MySerial.println(String(" fix took: ") + (millis() - start) + String("ms"));
+        MySerial.println(String(" time to find fix: ") + (millis() - start) + String("ms"));
         MySerial.println(String(" datetime = ") + sodaq_gps.getDateTimeString());
         MySerial.println(String(" lat = ") + String(sodaq_gps.getLat(), 7));
         MySerial.println(String(" lon = ") + String(sodaq_gps.getLon(), 7));
@@ -67,12 +96,6 @@ void loop()
     } else {
         MySerial.println("No Fix");
     }
-    uint32_t wait_ms = intervals[interval_ix];
-    if (++interval_ix > ARRAY_DIM(intervals)) {
-        interval_ix = 0;
-    }
-    MySerial.println(String("delay ...") + wait_ms + String("ms"));
-    delay(wait_ms);
 }
 
 void do_flash_led(int pin)
